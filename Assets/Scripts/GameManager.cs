dc37ad819967af85e255f0800d428de79a931edd
BlinkouzZ <francoagcaoili29@gmail.com>
2025-04-10 08:43:20 +0800

    GameManager
    Organized GameManager and coded dctrtrigger

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -22,25 +22,10 @@ public class GameManager : MonoBehaviour
         switch (questID)
         {
             case 0:
-                TextQuest.text = "Embark on your new journey!";
+                TextQuest.text = "Who's that?";
                 break;
             case 1:
-                TextQuest.text = "Oops! Get your backpack from your house.";
-                break;
-            case 2:
-                TextQuest.text = "Alright! Greet and talk to your master at his house.";
-                break;
-            case 3:
-                TextQuest.text = "Oh? Find the items he asked for.";
-                break;
-            case 4:
-                TextQuest.text = "Find the next item. ";
-                break;
-            case 5:
-                TextQuest.text = "Return to Master.";
-                break;
-            case 6:
-                TextQuest.text = "Basic Tutorial Completed!";
+                TextQuest.text = "I guess I'll do that.";
                 break;
             default:
                 TextQuest.text = "";
--- Assets/Scripts/GameScene/LvL1DctrTrigger.cs
+++ Assets/Scripts/GameScene/LvL1DctrTrigger.cs
@@ -1,8 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LvL1DctrTrigger : MonoBehaviour
 {
-    
+    public GameManager gameManager;
+
+    [Header("UI")]
+    public Text InteractMonologue;
+
+    [Header("Others")]
+    public GameObject interactionBlockerObject;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && GameManager.questID == 1)
+        {
+            InteractMonologue.text = "Press [F] to interact";
+        }
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && GameManager.questID == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                gameManager.questID = 2;
+                interactionBlockerObject.SetActive(false);
+                InteractMonologue.text = "";
+            }
+        }
+    }
 }
